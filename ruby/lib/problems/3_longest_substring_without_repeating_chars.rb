12e408e88556c1f3c151e689d2adceb546b8f8df
Hiroshi Ito <itohiro73@gmail.com>
2020-01-07 23:48:28 +0900

    Improve #3 Longest Substring Without Repeating Chars to use sliding window.

--- ruby/lib/problems/3_longest_substring_without_repeating_chars.rb
+++ ruby/lib/problems/3_longest_substring_without_repeating_chars.rb
@@ -1,39 +1,17 @@
 # @param {String} s
 # @return {Integer}
 def length_of_longest_substring(s)
-    existing_characters = {}
-    string = ""
-    longest_size = 0
-    size = 0
-    s.each_char{|c| 
-            if !existing_characters.has_key?(c) then
-                existing_characters[c] = size
-                string.concat(c)
-                size = size + 1
-            else
-                if (string.size > longest_size) then
-                    longest_size = string.size
-                end
-                index = existing_characters[c]
-                dropping_string = string.slice(0, index+1)
-                remaining_string = string.slice(index+1, string.size - (index+1))
-                dropping_string.each_char{|c| existing_characters.delete(c)}
-                remaining_index = 0
-                remaining_string.each_char{|c| 
-                    existing_characters[c] = remaining_index
-                    remaining_index = remaining_index + 1
-                }
-                string = remaining_string == nil ? "" : remaining_string
-                size = string.size
-                existing_characters[c] = size
-                string.concat(c)
-                size = size + 1
-            end
-        }
-        
-    if (string.size > longest_size) then
-        longest_size = string.size
-        string = ""
+    n = s.size
+    ans = 0;
+    hash = {}
+    i = 0;
+    chars = s.split("")
+    for j in 0..n-1 do
+        if hash.has_key?(chars[j]) then
+            i = [hash[chars[j]], i].max
+        end
+        ans = [ans, j - i + 1].max
+        hash[chars[j]] = j + 1
     end
-    return longest_size
+    return ans
 end
